a27222aa4861c1d1169d68ec334728a1e2d9e900
Chantri11 <esportschan10@gmail.com>
2024-10-01 23:11:47 -0500

    Implementación Dialogo
    Se creó el Script del NPC y el funcionamiento interactivo de dialogo con él.

---
+++ Assets/Codigos/NPC.cs
@@ -0,0 +1,101 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NPC : MonoBehaviour
+{
+    public GameObject panelDialogo;
+    public Text textoDialogo;
+    public string[] dialogo;
+    private int index;
+
+    public GameObject boton;
+    public float velocidadTexto;
+    public bool distanciaJugador;
+
+    // Update is called once per frame
+    void Start()
+    {
+        panelDialogo.SetActive(false);
+    }
+    
+    
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E) && distanciaJugador)
+        {
+            if(panelDialogo.activeInHierarchy)
+            {
+                ceroTexto();
+            }
+            else
+            {
+                panelDialogo.SetActive(true); 
+                StartCoroutine(Escritura());
+
+            }
+
+        }  
+
+        if(textoDialogo.text == dialogo[index])
+        {
+            boton.SetActive(true);
+        } 
+
+    }
+
+
+    public void ceroTexto()
+    {
+        textoDialogo.text = "";
+        index = 0;;
+        panelDialogo.SetActive(false);
+    }
+
+
+    IEnumerator Escritura()
+    {
+        foreach(char letter in dialogo[index].ToCharArray())
+        {
+            textoDialogo.text += letter;
+            yield return new WaitForSeconds(velocidadTexto);
+        }
+    }
+
+    public void sigLinea()
+    {
+
+        boton.SetActive(false);
+        if(index < dialogo.Length - 1)
+        {
+            index++;
+            textoDialogo.text = " ";
+            StartCoroutine(Escritura());
+        }
+        else
+        {
+            ceroTexto();
+        }
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D otro)
+    {
+        if(otro.CompareTag("Player"))
+        {
+            distanciaJugador = true;
+
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D otro)
+    {
+        if(otro.CompareTag("Player"))
+        {
+            distanciaJugador = false;
+            ceroTexto();
+
+        }
+    }
+}
